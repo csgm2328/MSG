a690a9d352c1d50617e0d3aa6c20749910f6525a
조용일 <cyi1207@naver.com>
2021-09-09 20:27:21 +0900

    [S05P21C101-79][BE] feat : 리뷰 삭제 기능 추가

--- PJT/backend/src/main/java/com/web/c101/review/ReviewDao.java
+++ PJT/backend/src/main/java/com/web/c101/review/ReviewDao.java
@@ -2,7 +2,10 @@ package com.web.c101.review;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ReviewDao extends JpaRepository<Review, String> {
 
+    Optional<Review> findByRid(String rid);
 
 }
