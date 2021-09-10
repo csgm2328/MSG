2d73398c62504a720f6a284ac682a6604e209f81
조용일 <cyi1207@naver.com>
2021-09-10 10:57:54 +0900

    [S05P21C101-80][BE] feat : 리뷰 목록 조회 - create frontend

--- PJT/backend/src/main/java/com/web/c101/review/ReviewController.java
+++ PJT/backend/src/main/java/com/web/c101/review/ReviewController.java
@@ -1,12 +1,16 @@
 package com.web.c101.review;
 
 import com.web.c101.BasicResponse;
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @ApiResponses(value = { @ApiResponse(code = 401, message = "Unauthorized", response = BasicResponse.class),
 @ApiResponse(code = 403, message = "Forbidden", response = BasicResponse.class),
@@ -22,7 +26,7 @@ public class ReviewController {
     ReviewService service;
 
     // 리뷰 작성
-    @PostMapping("/addReply")
+    @PostMapping("/addReview")
     @ApiOperation(value = "리뷰등록")
     public Object addReview(@RequestBody ReviewDto req){
         log.info("리뷰 등록");
@@ -40,7 +44,7 @@ public class ReviewController {
     }
 
     // 리뷰 삭제
-    @PutMapping("/deleteReply")
+    @PutMapping("/deleteReview")
     @ApiOperation(value = "리뷰삭제")
     public Object delReview(@RequestParam String rid) {
         log.info("리뷰 삭제");
@@ -52,6 +56,32 @@ public class ReviewController {
         } else {
             result.status = false;
             result.data = "fail";
+        }
+
+        return result;
+    }
+
+    // 리뷰 목록
+    @GetMapping("/reviewList")
+    @ApiOperation(value = "리뷰 목록")
+    public Object getReview(String mid) {
+
+        log.info("리뷰 삭제");
+        final BasicResponse result = new BasicResponse();
+
+        List<ReviewDto> list = service.getReview(mid);
+
+        if(list != null) {
+
+            result.status = true;
+            result.data = "success";
+            result.object = list;
+
+        } else {
+
+            result.status = false;
+            result.data = "fail";
+
         }
 
         return result;
