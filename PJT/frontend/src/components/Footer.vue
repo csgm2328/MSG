cefa97448eb2abc12c849ac021b57555e49470c8
조성표 <pyo7410@gmail.com>
2021-09-23 10:49:59 +0900

    [S05P21C101-121][FE] feat: Footer, favicon 추가
    2021-09-23

---
+++ PJT/frontend/src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <div
+    class="
+      flex
+      items-center
+      justify-center
+      md:justify-start
+      bg-msg-footer
+      h-12
+      left-0
+      bottom-0
+      w-full
+      fixed
+    "
+  >
+    <div class="flex ml-7">Copyright 2021. MSG. All right reserved</div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
