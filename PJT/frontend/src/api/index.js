f718df7c60705eb369d0466d0d5513afb5055cb0
조성표 <pyo7410@gmail.com>
2021-09-27 10:10:30 +0900

    [FE] fix: axios 요청 주소 수정
    2021-09-27

--- PJT/frontend/src/api/index.js
+++ PJT/frontend/src/api/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 function createAxiosService() {
   return axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: 'localhost: 8080',
     withCredentials: true,
     headers: {
       'Content-type': 'application/json',
