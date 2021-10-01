99a902446ad9814baf9725212ab5a6adcb9fcf6c
조용일 <cyi1207@naver.com>
2021-10-01 19:28:01 +0900

    hotfix: keyword 페이지 수정
    Former-commit-id: 161caa3866ec76e7fb928ceb34a59448f3df5a41

--- PJT/frontend/src/views/Keyword.vue
+++ PJT/frontend/src/views/Keyword.vue
@@ -1,82 +1,93 @@
 <template>
-  <div>
-    <div class="w-11/12 h-full">
-      <div class="flex w-full justify-start">
-        <div id="test2"
-          class="
-            w-1/6
-            h-auto
-            bg-white
-            border-2 border-blue-500 border-opacity-50
-            rounded-lg
-            flex
-            mr-2
-            justify-center
-          "
-        >
-          긍정 or 부정
+  <div class="flex flex-col h-screen w-auto">
+    <Header />
+    <div class="flex flex-col justify-center items-center flex-grow bg-blue-100 w-auto pt-10 pb-10">
+      <div class="w-11/12 h-full">
+        <div class="flex w-full justify-start">
+          <div
+            id="test2"
+            class="
+              w-1/6
+              h-auto
+              bg-white
+              border-2 border-blue-500 border-opacity-50
+              rounded-lg
+              flex
+              mr-2
+              justify-center
+            "
+          >
+            긍정 or 부정
+          </div>
         </div>
-      </div>
-      <div class="flex w-full h-1/2 mt-2 justify-start">
-        <div
-          class="
-            w-3/4
-            h-full
-            bg-white
-            border-2 border-blue-500 border-opacity-50
-            rounded-lg
-            pt-2
-            mr-2
-            flex
-            justify-center
-          "
-          ref="inbox"
-        >
+        <div class="flex w-full h-1/2 mt-2 justify-start">
+          <div
+            class="
+              w-3/4
+              h-full
+              bg-white
+              border-2 border-blue-500 border-opacity-50
+              rounded-lg
+              pt-2
+              mr-2
+              flex
+              justify-center
+            "
+            ref="inbox"
+          >
             <div id="word-cloud"></div>
+          </div>
+          <div
+            class="
+              w-1/4
+              h-full
+              bg-white
+              border-2 border-blue-500 border-opacity-50
+              rounded-lg
+              pt-2
+              flex
+              justify-center
+            "
+          >
+            키워드 순위표
+          </div>
         </div>
-        <div
-          class="
-            w-1/4
-            h-full
-            bg-white
-            border-2 border-blue-500 border-opacity-50
-            rounded-lg
-            pt-2
-            flex
-            justify-center
-          "
-        >
-          키워드 순위표
-        </div>
-      </div>
-      <!-- 차트 -->
-      <div class="flex w-full h-1/2 mt-2 justify-center">
-        <div
-          class="
-            w-full
-            h-2/3
-            bg-white
-            border-2 border-blue-500 border-opacity-50
-            rounded-lg
-            pt-2
-            mr-2
-            flex
-            justify-center
-          "
-        >
-          <div id="columnOrientedData"></div>
+        <!-- 차트 -->
+        <div class="flex w-full h-1/2 mt-2 justify-center">
+          <div
+            class="
+              w-full
+              h-2/3
+              bg-white
+              border-2 border-blue-500 border-opacity-50
+              rounded-lg
+              pt-2
+              mr-2
+              flex
+              justify-center
+            "
+          >
+            <div id="columnOrientedData"></div>
+          </div>
         </div>
       </div>
     </div>
+    <Footer />
   </div>
 </template>
 
 <script>
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
 import bb, { line } from "billboard.js";
 import "billboard.js/dist/theme/insight.css";
 
 export default {
   name: "Analysis",
+  components: {
+    Header,
+    Footer,
+  },
   data() {
     return {
       data: undefined,
@@ -108,7 +119,6 @@ export default {
       bindto: "#columnOrientedData",
     });
 
-      
     //   console.log(width, height)
     this.genLayout();
   },
@@ -117,22 +127,22 @@ export default {
       var d3 = require("d3"),
         cloud = require("d3-cloud");
 
-        var width = parseInt(this.$refs.inbox.clientWidth);
+      var width = parseInt(this.$refs.inbox.clientWidth);
       var height = parseInt(this.$refs.inbox.clientHeight);
       var layout = cloud()
-        .size([width,height])
+        .size([width, height])
         .words(
           [
             { text: "Hello", size: 20 },
-        { text: "Bye", size: 30 },
-        { text: "Good", size: 10 },
-        { text: "Bad", size: 25 },
-        { text: "Title", size: 50 },
-        { text: "Style", size: 40 },
-        { text: "Vue", size: 30 },
-        { text: "Javascript", size: 24 },
-        { text: "HTML", size: 10 },
-        { text: "CSS", size: 34 },
+            { text: "Bye", size: 30 },
+            { text: "Good", size: 10 },
+            { text: "Bad", size: 25 },
+            { text: "Title", size: 50 },
+            { text: "Style", size: 40 },
+            { text: "Vue", size: 30 },
+            { text: "Javascript", size: 24 },
+            { text: "HTML", size: 10 },
+            { text: "CSS", size: 34 },
           ].map(function (d) {
             return { text: d.text, size: d.size, test: "haha" };
           })
