1d985fbb2e2b4029c33fb6b44fe4790af7d0b03c
조성표 <pyo7410@gmail.com>
2021-10-01 18:07:30 +0900

    [S05P21C101-171][FE/BE] add: 누락된 파일 추가
    2021-10-01
    
    
    Former-commit-id: 5e6f36a11f2cfdc8ce2b17ecf514f3ae1013d5a0

--- PJT/frontend/src/store/header.js
+++ PJT/frontend/src/store/header.js
@@ -1,10 +1,14 @@
 const state = {
   isOpen: false,
+  type: '',
 };
 
 const getters = {
   isOpen(state) {
     return state.isOpen;
+  },
+  type(state) {
+    return state.type;
   },
   // get_id: (state) => state.id,
 };
@@ -13,11 +17,17 @@ const mutations = {
   TOGGLE_ISOPEN(state, payload) {
     state.isOpen = payload;
   },
+  SET_TYPE(state, payload) {
+    state.type = payload;
+  },
 };
 
 const actions = {
   toggle_isOpen(context, data) {
     context.commit('TOGGLE_ISOPEN', data);
+  },
+  set_type(context, data) {
+    context.commit('SET_TYPE', data);
   },
 };
 
--- PJT/frontend/src/store/index.js
+++ PJT/frontend/src/store/index.js
@@ -2,15 +2,17 @@ import Vuex from 'vuex';
 import createPersistedState from 'vuex-persistedstate';
 import user from './user';
 import header from './header';
+import loading from './loading';
 
 export default new Vuex.Store({
   modules: {
     user,
     header,
+    loading,
   },
   plugins: [
     createPersistedState({
-      paths: ['user', 'header'],
+      paths: ['user', 'header', 'loading'],
     }),
   ],
 });
